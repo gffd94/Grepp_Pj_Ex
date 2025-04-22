e9e7307f797ade9bc4f7da35d0cdaac6d995881c
STGRAM\gffd9 <29088981l@gmail.com>
2025-04-22 10:20:31 +0900

    프로제트 init 설정

---
+++ src/main/java/io/gffd94/exhandle/ExHandleApplication.java
@@ -0,0 +1,13 @@
+package io.gffd94.exhandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExHandleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExHandleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/gffd94/exhandle/ExHandleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gffd94.exhandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExHandleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
