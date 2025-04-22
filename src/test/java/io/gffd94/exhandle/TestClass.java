640a78e24d878da81948ebf8414f5588ed75c613
STGRAM\gffd9 <29088981l@gmail.com>
2025-04-22 10:33:14 +0900

    1차 테스트 완료

---
+++ src/test/java/io/gffd94/exhandle/TestClass.java
@@ -0,0 +1,17 @@
+package io.gffd94.exhandle;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    @Test
+    @DisplayName("1번 테스트")
+    void test_1() throws Exception {
+
+        String test = "1차 테스트";
+        System.out.println(test);
+
+    }
+
+}
